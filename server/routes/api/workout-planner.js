8759e414b3efe98e6fcd8413c5ee0de6d7b394fc
2dogsandaguy <Neverclear@ymail.com>
2023-12-04 19:33:14 -0500

    working on controllers

--- server/routes/api/workout-planner.js
+++ server/routes/api/workout-planner.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
 const {
+    createWeights,
+    getWeightsId,
+    deleteWeights
+} = require("../../controllers/weights-controllers");
+const {
   createCardio,
   getCardioById,
   deleteCardio,
-} = require("../../controllers/cardio-controller");
+} = require("../../controllers/cardio-controller.js");
 
 
 const { authMiddleware } = require('../../utils/auth');
@@ -17,6 +22,11 @@ router.route("/cardio").post(createCardio);
 
 router.route("/cardio/:id").get(getCardioById).delete(deleteCardio);
 
+
+router.route("/weights").post(createWeights);
+
+
+router.route("/weights/id").get(getWeightsId).delete(deleteWeights);
 
 
 module.exports = router;
